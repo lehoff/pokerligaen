a003f77b8c331c95eafb5b7eca70cca8d1aa2544
Torben Hoffmann <torben.lehoff@gmail.com>
2011-03-09 22:50:04 +0100

    March 2011 done

--- src/pl_tally.erl
+++ src/pl_tally.erl
@@ -2,21 +2,63 @@
 
 -compile(export_all).
 
+%% @todo: a function to project total pot for the year and winnings.
+%% @todo: a funciton to calculate total winnings per player.
+
 fine_price() -> 5.0.
     
 buyin_price() -> 60.0.
 
 rebuy_price() -> 30.0.
     
-payout_fraction() -> 0.7.    
+payout_fraction() -> 0.7.   
 
+multiplier_year() -> 
+    lists:sum ( [1,1,1.5, 1,1,1,1, 1.5,2] ).
 
+year_base_payouts(Pot) when Pot < 1200 ->
+    [{1,400},{2,200},{3,0}];
+year_base_payouts(Pot) when Pot >= 1200, Pot < 1700 ->
+    [{1,600},{2,400},{3,200}];
+year_base_payouts(Pot) when Pot >= 1700, Pot < 2300 ->
+    [{1,700},{2,500},{3,300},{4,200}];
+year_base_payouts(Pot) when Pot >= 2300, Pot < 2800 ->
+    [{1,800},{2,600},{3,400},{4,300},{5,200}];
+year_base_payouts(Pot) when Pot >= 2800 ->
+    [{1,900},{2,700},{3,500},{4,400},{5,300},{6,200}].
+
+year_fraction(1) -> 0.5;
+year_fraction(2) -> 0.25;
+year_fraction(3) -> 0.25;
+year_fraction(_) -> 0.
+    
+
+-type beverage() :: 'softdrink' | 'beer' | 'coffee'.
+-type amount() :: float().
+-type tally_entry() :: {'fine', non_neg_integer()} | {beverage(), non_neg_integer(), amount(), amount()}.
+
+-type event_name() :: {non_neg_integer(),1..12}.
+
+-spec event_entries(event_name()) -> [ tally_entry() ].			   
 event_entries({2010,10}) ->
     [{fine,10},{softdrink,6,5,3},{beer,6,7,3.5},{coffee,6,3,1}];
-
+event_entries({2010,11}) ->
+    [{fine,10},{softdrink,5,5,3},{beer,8,7,3.5},{coffee,0,3,1}];
+event_entries({2010,12}) ->
+    [{fine,19}];
+event_entries({2011,1}) ->
+    [{fine,7},{softdrink,5,10,2},{beer,8,10,3.5},{coffee,2,3,1}];
+event_entries({2011,2}) ->
+    [{fine,6},{softdrink,7,5,1.5},{beer,7,3.5}];
+event_entries({2011,3}) ->
+    [{fine,5},{softdrink,2,8,4},{beer,3,8,4}];
 
 event_entries(_) ->
     [].
+
+year_prices() ->
+    %% engraving + medals
+    215 + 140.
 
 calc_total() ->
     pl_results:init(),
@@ -28,7 +70,23 @@ calc_total() ->
 		     end,
 		     {0,0,0},
 		     EventTotals),
-    {total,Totals}.
+    EvtMulti = [ pl_scores:multiplier(E) || E <- Events ],
+    [{total,Totals},{event_totals,EventTotals},{multipliers,EvtMulti}].
+
+predict_year_total() ->
+    Total = calc_total(),
+    Multi = lists:sum( proplists:get_value(multipliers,Total) ),
+    Myear = multiplier_year(),
+    {_,_,Tally} = proplists:get_value(total,Total),
+    Ytotal = trunc((Tally / Multi) * Myear - year_prices()),
+    [{year_total,Ytotal},{payouts,year_payouts(Ytotal)}].
+
+year_payouts(Pot) ->
+    Base = year_base_payouts(Pot),
+    BaseTotal = lists:sum( [ P || {_,P} <- Base ] ),
+    Remainder = trunc ( (Pot - BaseTotal) div 40 ) * 40,
+    Extra = [ {No,Remainder*year_fraction(No)} || No <- [1,2,3] ],
+    [ {No, B + proplists:get_value(No,Extra,0)} || {No,B} <- Base ].
 
 calc_event(Evt) ->
     Es = event_entries(Evt),
